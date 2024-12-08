a8dfe9092ea14298a73ff57b88904efac4531f0b
Mikko Kouhia <mikko.kouhia@iki.fi>
2024-12-08 07:44:25 +0200

    Day 6 part 2

--- y24d06/src/main.rs
+++ y24d06/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::{
     collections::HashSet,
     io::{self, Read},
@@ -25,7 +26,12 @@ fn part1(input: &str) -> usize {
     let shape = (grid.len(), grid[0].len());
     let (pos, dir_i) = find_guard(&grid).expect("Guard not found");
 
-    walk_guard(&pos, dir_i, &shape, &grid).unwrap().len()
+    walk_guard(&pos, dir_i, &shape, &grid)
+        .unwrap()
+        .1
+        .iter()
+        .unique()
+        .count()
 }
 
 /// Perform guard walk
@@ -41,7 +47,7 @@ fn walk_guard(
     dir_i: usize,
     shape: &(usize, usize),
     grid: &[Vec<char>],
-) -> Option<Vec<(usize, usize)>> {
+) -> Option<(Vec<Vec<[bool; 4]>>, Vec<(usize, usize)>)> {
     let dirs = [
         (-1, 0), // UP
         (0, 1),  // RIGHT
@@ -56,8 +62,10 @@ fn walk_guard(
     let mut visited: Vec<Vec<[bool; 4]>> = (0..shape.0)
         .map(|_| (0..shape.1).map(|_| [false, false, false, false]).collect())
         .collect();
+    let mut path = Vec::new();
     loop {
         pos = (next.0, next.1);
+        path.push((pos.0 as usize, pos.1 as usize));
 
         if visited[pos.0 as usize][pos.1 as usize][dir_i] {
             // Been there, loop detected.
@@ -72,31 +80,10 @@ fn walk_guard(
         }
         if grid[next.0 as usize][next.1 as usize] == '#' {
             dir_i = (dir_i + 1) % 4;
-            next = (
-                (pos.0 as i32 + dirs[dir_i].0),
-                (pos.1 as i32 + dirs[dir_i].1),
-            );
+            next = pos.clone(); // Do not go forward, just turn
         }
     }
-    Some(
-        visited
-            .iter()
-            .enumerate()
-            .map(|(i, r)| {
-                r.iter()
-                    .enumerate()
-                    .filter_map(|(j, v)| {
-                        if !v.iter().any(|&x| x) {
-                            None
-                        } else {
-                            Some((i, j))
-                        }
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .flatten()
-            .collect(),
-    )
+    Some((visited, path))
 }
 
 /// Get guard position from the grid
@@ -115,8 +102,6 @@ fn find_guard(grid: &[Vec<char>]) -> Option<((usize, usize), usize)> {
 }
 
 /// How many possible blocking positions are there
-///
-/// 1723 - too high. Not passing.
 fn part2(input: &str) -> usize {
     let mut grid: Vec<Vec<char>> = input
         .lines()
@@ -126,8 +111,8 @@ fn part2(input: &str) -> usize {
     let (pos, dir_i) = find_guard(&grid).expect("Guard not found");
 
     let mut loop_blocks = HashSet::new();
-    let guard_path = walk_guard(&pos, dir_i, &shape, &grid).unwrap();
-    for next in guard_path[1..].iter() {
+    let (visited, guard_path) = walk_guard(&pos, dir_i, &shape, &grid).unwrap();
+    for next in guard_path[1..].iter().dedup() {
         grid[next.0][next.1] = '#';
         if walk_guard(&pos, dir_i, &shape, &grid).is_none() {
             loop_blocks.insert(next.clone());
@@ -155,6 +140,28 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(SAMPLE), 41);
+    }
+
+    #[test]
+    fn multiple_turns() {
+        let s = "
+..#...
+.....#
+..^.#."
+            .trim();
+        assert_eq!(part1(&s), 6);
+    }
+
+    /// Expect block to be placed at (0, 3)
+    #[test]
+    fn block_placement() {
+        let s = "
+#.....
+.....#
+^.#..#
+....#."
+            .trim();
+        assert_eq!(part2(&s), 1);
     }
 
     #[test]
